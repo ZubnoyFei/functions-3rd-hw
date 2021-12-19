1af0cfbd1b3226d9aa4eceb5e8afd497ff57ca6f
ZubnoyFei <93076661+ZubnoyFei@users.noreply.github.com>
2021-12-19 14:30:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function askUser() {
-    const Words = [];
+        const words = [];
     let answer = true;
-    for (let i = 0; true; i++) {
-        if (i % 3 === 0 && i !== 0) {
-            answer = confirm('Ready to continue?');
+    for (let i=0;true; i++) {
+        if(i%3===&& i!==0) {
+            answer = confirm ('Ready to continue?');
         }
-        if (!answer) {
+        if(!answer) {
             break;
         }
         const currentWord = prompt('Enter your word');
         words.push(currentWord);
     }
 }
-console.log(Words);
-askUser();
+ console.log(words);
+ askUser();
